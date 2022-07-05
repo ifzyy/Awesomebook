a34221bf62c5b254014f7f17d7db1310072b2df4
ifzyy <johnsonnifemi8@gmail.com>
2022-07-05 13:47:11 +0100

    module fix

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-import display from './modules/display.js';
-import dateNow from './modules/date.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import navigation from './modules/navigation.js';
+import display from './modules/display.js';
 
-dateNow();
-display();
+const date = document.getElementById('date');
+const displayDate = (date) => {
+  setInterval(() => {
+    date.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+  }, 1000);
+};
+displayDate(date);
 navigation();
+display();
--- modules/date.js
+++ modules/date.js
@@ -1,8 +1,8 @@
-import luxon from './luxon.js';
+// import luxon from './luxon.js';
 
-const dateSection = document.querySelector('.date');
-const dateNow = () => {
-  const dateTime = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL);
-  dateSection.innerHTML = dateTime;
-};
-export default dateNow();
+// const dateSection = document.querySelector('.date');
+// const dateNow = () => {
+//   const dateTime = luxon.DateTime.now().toLocaleString(luxon.DateTime.DATETIME_FULL);
+//   dateSection.innerHTML = dateTime;
+// };
+// export default dateNow();
